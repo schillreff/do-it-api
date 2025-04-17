3a13473eeace78af93ec9aeb95eb6c4f9711ba39
Leandro Schillreff <leandroschillreff@gmail.com>
2025-04-17 15:56:36 -0400

    refactor: Remove unused fields from NoteDTO class

--- src/main/java/dev/leandroschillreff/do_it/dto/NoteDTO.java
+++ src/main/java/dev/leandroschillreff/do_it/dto/NoteDTO.java
@@ -1,7 +1,5 @@
 package dev.leandroschillreff.do_it.dto;
 
-import java.time.LocalDateTime;
-
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,15 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NoteDTO {
-    private Long id;
-    
     @NotBlank(message = "Título não pode estar em branco")
     private String title;
-    
+
     @NotBlank(message = "Descrição não pode estar em branco")
     private String description;
-    
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private Long userId;
 }
