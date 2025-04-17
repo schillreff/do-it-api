1c24e09be0c35bf7fdb169e69983c5a210f4109e
Leandro Schillreff <leandroschillreff@gmail.com>
2025-04-17 15:57:05 -0400

    feat: Add NoteResponseDTO class for handling note response data

---
+++ src/main/java/dev/leandroschillreff/do_it/dto/NoteResponseDTO.java
@@ -0,0 +1,17 @@
+package dev.leandroschillreff.do_it.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoteResponseDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+}
