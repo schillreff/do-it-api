a798287af4acba1e380bb08dbbbf6724f011554f
Leandro Schillreff <leandroschillreff@gmail.com>
2025-04-23 13:47:18 -0400

    feat: Implement DotenvPostProcessor to load environment variables from .env file

---
+++ src/main/java/dev/leandroschillreff/do_it/config/DotenvPostProcessor.java
@@ -0,0 +1,16 @@
+package dev.leandroschillreff.do_it.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class DotenvPostProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
